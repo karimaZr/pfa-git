f88e19005ac030facfd18f2c01a3938d249171fd
Ikram Ed-damnati <eddmnatiikram123@gmail.com>
2023-05-02 15:42:17 +0100

    neww

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+use App\Models\Filiere;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    public function filiere()
+    {
+        return $this->belongsTo(Filiere::class);
+    }
 
     /**
      * The attributes that are mass assignable.
--- resources/views/etudiants/create.blade.php
+++ resources/views/etudiants/create.blade.php
@@ -1,31 +1,26 @@
-@include('backend.layouts.app')
-@include('backend.layouts.sidebar')
-@include('backend.layouts.navbar')
+@extends('backend.layouts.app')
+@section('content')
 <div class="container-fluid pt-4 px-4">
     <div class="row g-4 ">
         <div class="col-sm-12 col-xl-12">
         <h1 class="mb-5">Ajouter un nouvel étudiant</h1>
-        <form method="post" action="{{ route('etudiants.store') }}">
+        <form method="post" action="{{ route('etudiants.store') }}" enctype="multipart/form-data">
             @csrf
             <div class="form-group">
-                <label for="nom">Nom</label>
-                <input type="text" class="form-control" id="nom" name="nom" required>
-            </div>
-            <div class="form-group">
-                <label for="prenom">Prénom</label>
-                <input type="text" class="form-control" id="prenom" name="prenom" required>
+                <label for="name">Nom</label>
+                <input type="text" class="form-control" id="name" name="name" required>
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
                 <input type="email" class="form-control" id="email" name="email" required>
             </div>
             <div class="form-group">
-                <label for="mot_de_passe">Mot de passe</label>
-                <input type="password" class="form-control" id="mot_de_passe" name="mot_de_passe" required>
+                <label for="password">Mot de passe</label>
+                <input type="password" class="form-control" id="password" name="password" required>
             </div>
             <div class="form-group">
-                <label for="cne">CNE</label>
-                <input type="text" class="form-control" id="cne" name="cne" required>
+                <label for="CNE">CNE</label>
+                <input type="text" class="form-control" id="CNE" name="CNE" required>
             </div>
             <div class="form-group">
                 <label for="date_de_naissance">Date de naissance</label>
@@ -39,8 +34,12 @@
                     @endforeach
                 </select>
             </div>
+            <div class="form-group">
+                <label for="photo">Photo</label>
+                <input type="file" class="form-control" id="photo" name="photo">
+            </div>
             <button type="submit" class="btn btn-primary">Enregistrer</button>
         </form>
     </div>
     </div>
- @include('backend.layouts.footer')       
+    @endsection
